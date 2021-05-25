2fac64ecd6bb299b59fac0d90c9fe24de99827c2
Markus Ritzer <markus.ritzer@student.tugraz.at>
2021-05-25 15:35:39 +0200

    Made ListViews scrollable

--- app/src/main/java/eu/faircode/netguard/ActivityCipher.java
+++ app/src/main/java/eu/faircode/netguard/ActivityCipher.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -79,5 +81,28 @@ public class ActivityCipher extends AppCompatActivity {
         lvSuits.setAdapter(suitsAdapter);
         lvProtocols.setAdapter(protocolsAdapter);
 
+        ListUtils.setDynamicHeight(lvSuits);
+        ListUtils.setDynamicHeight(lvProtocols);
+    }
+
+    public static class ListUtils {
+        public static void setDynamicHeight(ListView mListView) {
+            ListAdapter mListAdapter = mListView.getAdapter();
+            if (mListAdapter == null) {
+                // when adapter is null
+                return;
+            }
+            int height = 0;
+            int desiredWidth = View.MeasureSpec.makeMeasureSpec(mListView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+            for (int i = 0; i < mListAdapter.getCount(); i++) {
+                View listItem = mListAdapter.getView(i, null, mListView);
+                listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+                height += listItem.getMeasuredHeight();
+            }
+            ViewGroup.LayoutParams params = mListView.getLayoutParams();
+            params.height = height + (mListView.getDividerHeight() * (mListAdapter.getCount() - 1));
+            mListView.setLayoutParams(params);
+            mListView.requestLayout();
+        }
     }
 }
